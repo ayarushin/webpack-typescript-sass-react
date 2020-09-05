68eb7029547a84687e3f4671d377adc0ed21d15e
Alexey Yarushin <yarushin.dev@gmail.com>
2020-09-05 15:49:51 +0300

    Update template

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { App } from './components/App/app';
+import App from './components/App/app';
 
 import './styles/index.scss';
 
